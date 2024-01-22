dc249fee5a26a1d5165cd794b44ea6cdeea71635
Michael Wright <mikeywri2000@gmail.com>
2024-01-22 17:53:01 +0000

    think im finished for the day

--- day7/main.c
+++ day7/main.c
@@ -2,10 +2,55 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../helper.h"
+#include <stdbool.h>
 
 #define INIT_SIZE 10
 
+typedef struct{
+    int count[5];
+    char card[5];
+    int cardNumber;
+}hashmap;
 
+void initHashMap(hashmap *h){
+    for(int i = 0; i < 5; i++){
+        h->count[i] = 0;
+        h->card[i] = 'X';
+    }
+    h->cardNumber = 0;
+}
+
+
+void computeHand(char *hand){
+
+    hashmap handHash;
+    initHashMap(&handHash);
+
+    bool new;
+    for(int i = 0; i < 5; i++){
+        new = true;
+        for(int j = 0; j < 5; j++){
+            if(hand[i] == handHash.card[j]){
+                handHash.count[j]++;
+                new = false;
+                break;
+            } 
+        }
+        if (new){
+            handHash.card[handHash.cardNumber] = hand[i];
+            handHash.count[handHash.cardNumber] = 1;
+            handHash.cardNumber++;
+
+        }
+    }
+
+    //Try a cool trick with squares, cubes and Quaruples?!?! -- use smallest int as 1000
+    //Should work would be cool to check tho -- dont forget long int!!
+
+    for(int k = 0; k < 5; k++){
+        printf("Count = %d, Card = %c |\n ", handHash.count[k], handHash.card[k]);
+    }   
+}
 
 
 int main(){ 
@@ -22,6 +67,8 @@ int main(){
     size_t len = 0;
     ssize_t read;
 
+    //char strength[13] = {'2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'};
+
     Array hands;
     Array bids;
 
@@ -37,6 +84,8 @@ int main(){
 
     for(int i = 0; i < hands.used;i++){
         printf("%s\n",hands.sArray[i]);
+        computeHand(hands.sArray[i]);
+        printf("\n\n");
     }
 
 
