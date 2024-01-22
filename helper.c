f2f5da3a204245d36597b8864f9339eebbfabf6b
Michael Wright <mikeywri2000@gmail.com>
2024-01-22 15:01:20 +0000

    forgot the helper files

--- helper.c
+++ helper.c
@@ -2,6 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct {
+  long int *array;
+  size_t used;
+  size_t size;
+} Array;
+
+void initArray(Array *a, size_t initialSize) {
+  a->array = malloc(initialSize * sizeof(long int));
+  a->used = 0;
+  a->size = initialSize;
+}
+
+void insertArray(Array *a, long int element) {
+  // a->used is the number of used entries, because a->array[a->used++] updates a->used only *after* the array has been accessed.
+  // Therefore a->used can go up to a->size 
+  if (a->used == a->size) {
+    a->size *= 2;
+    a->array = realloc(a->array, a->size * sizeof(long int));
+  }
+  a->array[a->used++] = element;
+}
+
+void freeArray(Array *a) {
+  free(a->array);
+  a->array = NULL;
+  a->used = a->size = 0;
+}
+
+
 long int *get_array_numbers(char *numbers, int size){
     
     long int * values = (long int*) malloc((size + 1) * sizeof(long int));
@@ -16,4 +45,19 @@ long int *get_array_numbers(char *numbers, int size){
     values[index] = -1;
 
     return values;
+}
+
+
+void fill_array_numbers(char *numbers,Array *myarr, size_t size){
+
+
+    initArray(myarr,size);
+    char * num_token = strtok(numbers, " ");
+
+
+    while(num_token != NULL){
+        insertArray(myarr, atol(num_token));
+        num_token = strtok(NULL, " ");
+    }
+
 }
--- helper.h
+++ helper.h
@@ -1,1 +1,15 @@
+typedef struct {
+  long int *array;
+  size_t used;
+  size_t size;
+} Array;
+
+void initArray(Array *a, size_t initialSize);
+
+void insertArray(Array *a, long int element);
+
+void freeArray(Array *a);
+
 long int *get_array_numbers(char *numbers,int size);
+
+void fill_array_numbers(char *numbers,Array *myarr, size_t size);
